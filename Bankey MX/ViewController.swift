235212363d7c47909d25dc05718d65289f17acae
manueldevmx <103620103+EcoticketApp@users.noreply.github.com>
2022-11-21 15:56:39 -0600

    Initial Commit

---
+++ Bankey MX/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bankey MX
+//
+//  Created by Manuel Hernandez De la Cruz on 21/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Bankey MXUITests/Bankey_MXUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Bankey_MXUITestsLaunchTests.swift
+//  Bankey MXUITests
+//
+//  Created by Manuel Hernandez De la Cruz on 21/11/22.
+//
+
+import XCTest
+
+class Bankey_MXUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
